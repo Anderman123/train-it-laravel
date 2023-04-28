e50d07df76df8e5de24dbc99b29b55f07a6a2f7d
alexander <aloj@fp.insjoaquimmir.cat>
2023-04-28 17:58:39 +0200

    migracion lograda con backend

--- TRAINIT/database/migrations/2023_04_28_143436_create_guardado_table.php
+++ TRAINIT/database/migrations/2023_04_28_143436_create_guardado_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('guardados', function (Blueprint $table) {
+            // hola
             $table->id();
             $table->unsignedBigInteger('usuario_id');
             $table->unsignedBigInteger('post_id');
